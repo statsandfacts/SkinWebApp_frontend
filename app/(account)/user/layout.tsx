f1f4c11994851d90ba99f6a2ebc79853b68fe651
Akashpradhan-Git <akashpradhan151@gmail.com>
2024-05-15 19:08:11 +0530

    Some bug fixed

--- app/(account)/user/layout.tsx
+++ app/(account)/user/layout.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import AuthProvider from '../../AuthProvider';
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <>
-        <div className='bg-gradient-to-r from-[#9DEAF4]  to-[#F0936C] min-h-screen'>
-          {children}
-        </div>
-      </>
+      <AuthProvider>
+        <>{children}</>
+      </AuthProvider>
     </>
   );
 };
