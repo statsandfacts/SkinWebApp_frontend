20fc203b82b7885616892daf3157bd09a38c6a55
statsandfacts <157334892+statsandfacts@users.noreply.github.com>
2024-05-29 01:05:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/user',
-    assetPrefix: '/user',
+   /** basePath: '/user',
+    assetPrefix: '/user', */
     reactStrictMode: true,
     images: {
         domains: ['nextcare.s3.us-east-2.amazonaws.com'],
