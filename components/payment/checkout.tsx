a30a112a02c1d63bea82294b7885c4c5f9e35ffb
Akashpradhan-Git <akashpradhan151@gmail.com>
2024-06-19 00:18:14 +0530

    payment

--- components/payment/checkout.tsx
+++ components/payment/checkout.tsx
@@ -28,7 +28,6 @@ import {
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import Link from 'next/link';
-import PaymentLoader from '../Skeleton/PaymentLoader';
 
 const Checkout = () => {
   const router = useRouter();
