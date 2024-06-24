37e694702069f91619b7720e98548d675e1ebd07
sambeetmandal <mandalsambeet94@gmail.com>
2024-06-24 20:32:43 +0530

    access key changes

--- utils/s3Upload.ts
+++ utils/s3Upload.ts
@@ -5,7 +5,7 @@ const REGION = 'us-east-2'; // Replace with your region
 export const s3Client = new S3Client({
   region: REGION,
   credentials: {
-    accessKeyId: 'AKIAZI2LCNUH7TCAU7XU',
-    secretAccessKey: 'IPfV7fxJHyRwOY54PHbqanocBoBvUAg9Dj54HLTH',
+    accessKeyId: 'AKIAZI2LCNUHYEQ4B67A',
+    secretAccessKey: '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',
   },
 });
