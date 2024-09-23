91da2e87a69713793b86380fb4d04fa9c58e9200
Kanha Kumar Khatua <kanha@rixosys.com>
2024-09-23 21:06:27 +0530

    style: design modified stage 1

--- components/DigitalPrescription/Details/PrescriptionDetails.tsx
+++ components/DigitalPrescription/Details/PrescriptionDetails.tsx
@@ -1,9 +1,6 @@
 "use client";
 import React, { useEffect } from "react";
-import {
-  Accordion,
-  AccordionItem,
-} from "@nextui-org/react";
+import { Accordion, AccordionItem } from "@nextui-org/react";
 import {
   EyeIcon,
   DocumentMagnifyingGlassIcon,
@@ -22,7 +19,6 @@ import { AppDispatch, RootState } from "@/redux/store";
 import { useAuthInfo } from "@/hooks/useAuthInfo";
 import { fetchPatientDashboard } from "@/redux/slices/digitalPrescription/userDashboard.slice";
 
-
 const PrescriptionDetails: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { userId } = useAuthInfo();
@@ -35,6 +31,8 @@ const PrescriptionDetails: React.FC = () => {
     dispatch(fetchPatientDashboard(userId));
   }, [dispatch, userId]);
 
+  console.log("dashboardData", dashboardData);
+
   return (
     <>
       <Accordion variant="splitted">
@@ -42,35 +40,50 @@ const PrescriptionDetails: React.FC = () => {
           dashboardData?.patient_case_dtls.map((cases: any, cx: number) => (
             <AccordionItem
               key={cx}
-              aria-label={dashboardData?.name + cx}
-              title={dashboardData?.name}
+              aria-label={cases?.case_id + cx}
+              title={`Status: ${cases?.status}`}
             >
               <React.Fragment>
                 <table className="w-full">
+                  <thead>
+                    <tr>
+                      <th>Doctor Name</th>
+                      <th>Date</th>
+                      <th>Address</th>
+                      <th></th>
+                    </tr>
+                  </thead>
                   <tbody>
                     {cases?.prescription_dtls.length > 0 &&
                       cases?.prescription_dtls.map(
                         (prescription: any, pi: number) => (
                           <tr key={pi}>
-                            <td> {prescription?.provider_name} </td>
+                            <td> {prescription?.doctor_name} </td>
                             <td> {prescription?.prescription_date} </td>
                             <td> {prescription?.provider_dtls} </td>
-                            <td className="flex gap-4 justify-center" >
+                            <td className="flex gap-4 justify-center">
+                              {cases?.status === "approve" && (
+                                <ToolTipBtn
+                                  onClick={() => {
+                                    dispatch(
+                                      setSinglePrescriptionDetails(prescription)
+                                    );
+                                    dispatch(
+                                      setViewPrescriptionDetailsModal(true)
+                                    );
+                                  }}
+                                  title="View Prescription Details"
+                                  key={1}
+                                >
+                                  <DocumentMagnifyingGlassIcon className="h-5 w-5" />
+                                </ToolTipBtn>
+                              )}
+
                               <ToolTipBtn
                                 onClick={() => {
-                                  dispatch(setSinglePrescriptionDetails(prescription));
                                   dispatch(
-                                    setViewPrescriptionDetailsModal(true)
+                                    setSinglePrescriptionDetails(prescription)
                                   );
-                                }}
-                                title="View Prescription Details"
-                                key={1}
-                              >
-                                <DocumentMagnifyingGlassIcon className="h-5 w-5" />
-                              </ToolTipBtn>
-                              <ToolTipBtn
-                                onClick={() => {
-                                  dispatch(setSinglePrescriptionDetails(prescription));
                                   dispatch(setViewOriginalImageModal(true));
                                 }}
                                 title="View Original Image"
@@ -78,13 +91,6 @@ const PrescriptionDetails: React.FC = () => {
                               >
                                 <EyeIcon className="h-5 w-5" />
                               </ToolTipBtn>
-                              {/* <ToolTipBtn
-                                onClick={() => {}}
-                                title="Download Digital Prescription"
-                                key={3}
-                              >
-                                <ArrowDownTrayIcon className="h-5 w-5" />
-                              </ToolTipBtn> */}
                             </td>
                           </tr>
                         )
