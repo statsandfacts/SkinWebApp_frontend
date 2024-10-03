3caaa84ef559e4ae8afbe789cb15b8ca08a8b761
Kanha Kumar Khatua <kanha@rixosys.com>
2024-10-03 18:26:46 +0530

    page loading added

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-sky-500"></div>
+    </div>
+  );
+};
+
+export default Loading;
