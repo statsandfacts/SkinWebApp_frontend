658066ec0bce96a2c82ffef0139f768b13bf3af2
Kanha Kumar Khatua <kanha@rixosys.com>
2024-11-28 20:45:51 +0530

    fix: forgot password to rename reset phone

--- components/DigitalPrescription/Auth/LoginDrawer.tsx
+++ components/DigitalPrescription/Auth/LoginDrawer.tsx
@@ -182,7 +182,7 @@ export default function LoginDrawer() {
                           dispatch(setLoginModal(false));
                         }}
                       >
-                        Forget Password?
+                        Reset Phone Number ?
                       </button>
                     </div>
                   </div>
