e3b3c2482c00fa482e95ea84da61b233ea85366f
Kanha Kumar Khatua <kanha@rixosys.com>
2025-01-13 18:53:39 +0530

    Add: Implement FollowUsBlogComponent and refactor BlogSegregation for improved layout and readability

--- app/blog/_component/BlogSegregation.tsx
+++ app/blog/_component/BlogSegregation.tsx
@@ -5,15 +5,7 @@ import {
   setReduxCatData,
 } from "@/redux/slices/digitalPrescription/blog.slice";
 import { AppDispatch, RootState } from "@/redux/store";
-import {
-  ChevronRight,
-  Facebook,
-  Instagram,
-  Linkedin,
-  Loader,
-  Twitter,
-  Youtube,
-} from "lucide-react";
+import { ChevronRight, Loader } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -21,6 +13,7 @@ import LatestBlogCarousel from "./LatestBlogCarousel";
 import Image from "next/image";
 import BlogItem from "./BlogItem";
 import { useRouter } from "next/navigation";
+import FollowUsBlogComponent from "./FollowUsBlogComponent";
 
 const BlogSegregation = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -53,7 +46,7 @@ const BlogSegregation = () => {
         ) : (
           <>
             <div className="w-full flex flex-col md:flex-row gap-3 md:gap-12">
-              <div className="w-2/3">
+              <div className="w-full md:w-2/3">
                 <LatestBlogCarousel
                   data={
                     allBlogsByCategories.data &&
@@ -90,8 +83,10 @@ const BlogSegregation = () => {
                               ) : (
                                 <div className="h-40"></div>
                               )}
-                              <p className="text-gray-700 font-semibold text-lg">
-                                {item?.title}
+                              <p className="text-gray-700 font-semibold text-base md:text-lg">
+                                {item?.title?.length > 30
+                                  ? `${item?.title?.slice(0, 30)}...`
+                                  : item?.title}
                               </p>
                             </div>
                           </Link>
@@ -99,89 +94,84 @@ const BlogSegregation = () => {
                   </div>
                 </div>
 
-                <div className="shadow-2xl p-4 mt-6 rounded-lg">
-                  <p className="text-gray-700 font-semibold text-2xl">
-                    Follow Us
-                  </p>
-                  <div className="flex gap-4 items-center py-2">
-                    <Link
-                      href="https://www.linkedin.com/company/nextcare-life/"
-                      className="p-2 rounded-full bg-sky-600 text-white hover:bg-sky-700 transition"
-                    >
-                      <Linkedin className="h-4 w-4" />
-                    </Link>
-                    <Link
-                      href="https://twitter.com/NextcareLife"
-                      className="p-2 rounded-full bg-sky-600 text-white hover:bg-sky-700 transition"
-                    >
-                      <Twitter className="h-4 w-4" />
-                    </Link>
-                    <Link
-                      href="https://www.instagram.com/nextcare.life?igsh=MTdkMjg5M2s2NmRobA%3D%3D&utm_source=qr"
-                      className="p-2 rounded-full bg-sky-600 text-white hover:bg-sky-700 transition"
-                    >
-                      <Instagram className="h-4 w-4" />
-                    </Link>
-                    <Link
-                      href="https://www.facebook.com/share/19dqeCuNou/?mibextid=wwXIfr"
-                      className="p-2 rounded-full bg-sky-600 text-white hover:bg-sky-700 transition"
-                    >
-                      <Facebook className="h-4 w-4" />
-                    </Link>
-                    <Link
-                      href="https://www.youtube.com/@NCL24283"
-                      className="p-2 rounded-full bg-sky-600 text-white hover:bg-sky-700 transition"
-                    >
-                      <Youtube className="h-4 w-4" />
-                    </Link>
-                  </div>
+                <div className="hidden md:flex md:flex-col">
+                  <FollowUsBlogComponent />
                 </div>
               </div>
             </div>
 
-            <div className="mt-6">
-              <div className="flex flex-col gap-4 animate-slide-up">
-                {allBlogsByCategories.data &&
-                  allBlogsByCategories.data.categories &&
-                  allBlogsByCategories.data.categories.map(
-                    (cat: any, index: number) => {
-                      const allBlogs =
-                        cat?.sub_categories?.flatMap(
-                          (subCat: any) => subCat?.blogs || []
-                        ) || [];
+            <div className="mt-6 md:flex flex-col gap-4 animate-slide-up">
+              {allBlogsByCategories.data &&
+                allBlogsByCategories.data.categories &&
+                allBlogsByCategories.data.categories.map(
+                  (cat: any, index: number) => {
+                    const allBlogs =
+                      cat?.sub_categories?.flatMap(
+                        (subCat: any) => subCat?.blogs || []
+                      ) || [];
 
-                      const topBlogs = allBlogs.slice(0, 3);
+                    const topBlogs = allBlogs.slice(0, 3);
 
-                      return (
-                        <div key={index}>
-                          <div className="flex justify-between">
-                            <p className="text-sky-800 text-xl font-medium">
-                              {cat?.name}
-                            </p>
-                            <button
-                              onClick={() => {
-                                dispatch(setReduxCatData(cat));
-                                router.push(
-                                  `/blog/category/${cat.category_id}`
-                                );
-                              }}
-                              className="text-sky-800 flex items-center transition ease-in-out duration-200 hover:translate-x-1 hover:text-sky-700"
-                            >
-                              View More
-                              <ChevronRight className="w-4 h-4 text-sky-800 transition-transform duration-200 ease-in-out hover:translate-x-1" />
-                            </button>
-                          </div>
-
-                          <div className="grid gap-5 grid-cols-1 md:grid-cols-3 mt-2">
-                            {topBlogs.map((blog: any, blogIndex: number) => (
-                              <BlogItem blog={blog} key={blogIndex} />
-                            ))}
-                          </div>
+                    return (
+                      <div key={index}>
+                        <div className="flex justify-between mt-4">
+                          <p className="text-sky-800 text-base md:text-xl font-medium">
+                            {cat?.name}
+                          </p>
+                          <button
+                            onClick={() => {
+                              dispatch(setReduxCatData(cat));
+                              router.push(`/blog/category/${cat.category_id}`);
+                            }}
+                            className="text-sky-800 flex items-center text-sm md:text-xl transition ease-in-out duration-200 hover:translate-x-1 hover:text-sky-700"
+                          >
+                            View More
+                            <ChevronRight className="w-4 h-4 text-sky-800 transition-transform duration-200 ease-in-out hover:translate-x-1" />
+                          </button>
                         </div>
-                      );
-                    }
-                  )}
-              </div>
+
+                        <div className="hidden md:grid gap-5 grid-cols-1 md:grid-cols-3 mt-2">
+                          {topBlogs.map((blog: any, blogIndex: number) => (
+                            <BlogItem blog={blog} key={blogIndex} />
+                          ))}
+                        </div>
+
+                        <div className="flex flex-col gap-4 md:hidden">
+                          {topBlogs.map((blog: any, blogIndex: number) => (
+                            <Link
+                              href={`/blog/${blog.blog_id}`}
+                              key={blogIndex}
+                              className="hover:bg-sky-50 p-2 rounded-lg h-fit"
+                            >
+                              <div className="flex gap-4">
+                                {blog?.image ? (
+                                  <Image
+                                    src={blog?.image}
+                                    alt={blog?.title}
+                                    width={150}
+                                    height={150}
+                                    className="rounded-lg"
+                                  />
+                                ) : (
+                                  <div className="h-40"></div>
+                                )}
+                                <p className="text-gray-700 font-semibold text-base md:text-lg">
+                                  {blog?.title?.length > 45
+                                    ? `${blog?.title?.slice(0, 45)}...`
+                                    : blog?.title}
+                                </p>
+                              </div>
+                            </Link>
+                          ))}
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
+            </div>
+
+            <div className="md:hidden">
+              <FollowUsBlogComponent />
             </div>
           </>
         )}
