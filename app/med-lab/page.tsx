1713939052687d54759cc8744b3827be33b405a6
kanhapro <kanhakumarkhatua2001@gmail.com>
2025-02-12 13:24:04 +0530

    fix: meds and labs seperation

--- app/med-lab/page.tsx
+++ app/med-lab/page.tsx
@@ -1,10 +1,10 @@
+import CustomHeader from "@/components/Header/PublicLayoutHeader";
 import SearchComponent from "./_component/SearchComponent";
-import HeaderPart from "./_component/HeaderPart";
 
 const HomePage = () => {
   return (
     <div className="p-10 md:px-40">
-      <HeaderPart
+      <CustomHeader
         header="Find Medicines & Investigations"
         subHeader="Search for medicines and lab investigations easily."
         imageURL="/vector/health_feed.png"
