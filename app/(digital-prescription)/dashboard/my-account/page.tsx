a1bdcfc1ce17cea86371f46282baae92bda4ac72
Guru-prasadrana <guruprasadrana571@gmail.com>
2025-02-25 14:10:48 +0530

    adddashboardHeader

--- app/(digital-prescription)/dashboard/my-account/page.tsx
+++ app/(digital-prescription)/dashboard/my-account/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { User, Phone, Mail } from "lucide-react";
 
-import MyAccount from "../_components/MyAccount";
+import DashboardHeader from "../_components/DashboardHeader";
 import ItemCard from "../_components/ItemCard";
 export const metadata: Metadata = {
   title: "AccountDetails",
@@ -28,7 +28,7 @@ const MyAccountPage = () => {
 
   return (
     <div className="flex flex-col items-center bg-white pb-8">
-      <MyAccount />
+      <DashboardHeader />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl px-4">
         {items.map((item, index) => (
