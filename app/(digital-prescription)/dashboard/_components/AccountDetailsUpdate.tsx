0d7e76da9c2915b190a2d0df8842504c9ceead84
@kanhapro <kanhakumarkhatua2001@gmail.com>
2025-03-26 14:23:16 +0530

    feat: enhance account details update with image upload functionality

--- app/(digital-prescription)/dashboard/_components/AccountDetailsUpdate.tsx
+++ app/(digital-prescription)/dashboard/_components/AccountDetailsUpdate.tsx
@@ -5,45 +5,50 @@ import { User } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
 import { fetchUserDetails } from "@/redux/slices/digitalPrescription/auth.slice";
-import { updateUser } from "@/services/api.digitalPrescription.service";
+import {
+  updateUser,
+  uploadImageToAws,
+} from "@/services/api.digitalPrescription.service";
 import { toast } from "react-toastify";
-import { BadgeCheck } from 'lucide-react';
-
+import { BadgeCheck } from "lucide-react";
+import dayjs from "dayjs";
+import { Button } from "@nextui-org/button";
 
 const AccountDetailsUpdate = () => {
   const dispatch = useDispatch<AppDispatch>();
   const userId = useSelector((state: RootState) => state.auth.userId);
   const userDetails = useSelector((state: RootState) => state.auth.userDetails);
 
-  const [profilePic, setProfilePic] = useState<string | null>(null);
   const [formData, setFormData] = useState({
     fullName: "",
     dob: "",
     gender: "",
     maritalStatus: "",
     zipCode: "",
-    email:"",
+    email: "",
+    profilePic: "",
   });
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [uILoading, setUILoading] = useState<boolean>(false);
 
-  // Fetch user details when the component mounts
   useEffect(() => {
     if (userId) {
       dispatch(fetchUserDetails(userId));
     }
   }, [dispatch, userId]);
 
-  // Populate form fields when userDetails updates
   useEffect(() => {
     if (userDetails) {
-      setFormData({
+      setFormData((prev) => ({
+        ...prev,
         fullName: userDetails.name || "",
-        dob: userDetails.dob || "",
+        dob: userDetails.dob ? dayjs(userDetails.dob).format("YYYY-MM-DD") : "",
         gender: userDetails.gender || "",
-        maritalStatus: userDetails.maritalStatus || "",
-        zipCode: userDetails.zipCode || "",
-        email:userDetails.email||"",
-      });
+        maritalStatus: userDetails.marital_status || "",
+        zipCode: userDetails.zipcode || "",
+        email: userDetails.email || "",
+        profilePic: userDetails.user_profile_image_path || "",
+      }));
     }
   }, [userDetails]);
 
@@ -56,23 +61,54 @@ const AccountDetailsUpdate = () => {
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       const file = e.target.files[0];
-      const imageUrl = URL.createObjectURL(file);
-      setProfilePic(imageUrl);
+      const formData = new FormData();
+      formData.append("f_name", "profile_pictures");
+      formData.append("files", file);
+      formData.append("phone_no", userDetails?.phone_no);
+      formData.append("doc_types", "profile_pictures");
+      setUILoading(true);
+      uploadImageToAws(formData)
+        .then((response) => {
+          const uploaded_file = response?.uploaded_files?.[0]?.file_url;
+          if (!uploaded_file) {
+            toast.error("File upload failed. Please try again.");
+            return;
+          }
+          setFormData((prev) => ({
+            ...prev,
+            profilePic: uploaded_file || "",
+          }));
 
-      const reader = new FileReader();
-      reader.onload = () => {
-        setFormData((prev) => ({
-          ...prev,
-          profilePic: reader.result as string,
-        }));
-      };
-      reader.readAsDataURL(file);
+          updateUser({
+            user_id: userDetails?.user_id,
+            user_profile_image_path: uploaded_file,
+          })
+            .then((response) => {
+              toast.success(
+                response.message || "User details updated successfully!"
+              );
+            })
+            .catch((error) => {
+              const errorMessage =
+                error.response?.data?.message || "Failed to update profile.";
+              toast.error(errorMessage);
+            });
+        })
+        .catch((error) => {
+          toast.error(
+            error.response?.data?.message || "Failed to upload image."
+          );
+        })
+        .finally(() => {
+          setUILoading(false);
+        });
+    } else {
+      toast.warning("No file selected.");
+      return;
     }
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const handleSubmit = () => {
     if (
       !formData.fullName ||
       !formData.dob ||
@@ -88,28 +124,26 @@ const AccountDetailsUpdate = () => {
     const payload = {
       user_id: userDetails?.user_id,
       name: formData.fullName,
-      date_of_birth: formData.dob,
+      dob: dayjs(formData.dob).format("MM/DD/YYYY"),
       gender: formData.gender,
       zipcode: formData.zipCode,
+      marital_status: formData.maritalStatus,
+      email: formData.email,
+      user_profile_image_path: formData.profilePic,
     };
 
-    try {
-      const response = await updateUser(payload);
-
-      if (response?.success) {
-        toast.success(response.message || "Profile updated successfully!");
-        console.log(response);
-      } else {
-        toast.error(response?.message || "Failed to update profile.");
-        console.log(response);
-      }
-    } catch (error: any) {
-      const errorMessage =
-        error.response?.data?.message || "Failed to update profile.";
-      toast.error(errorMessage);
-    } finally {
-      setLoading(false);
-    }
+    updateUser(payload)
+      .then((response) => {
+        toast.success(response.message || "User details updated successfully!");
+      })
+      .catch((error) => {
+        const errorMessage =
+          error.response?.data?.message || "Failed to update profile.";
+        toast.error(errorMessage);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
@@ -118,9 +152,9 @@ const AccountDetailsUpdate = () => {
         {/* Profile Image Section */}
         <div className="grid justify-center">
           <div className="w-60 h-60 overflow-hidden border relative rounded-lg">
-            {profilePic ? (
+            {formData.profilePic ? (
               <Image
-                src={profilePic}
+                src={formData.profilePic}
                 alt="Profile"
                 layout="fill"
                 objectFit="cover"
@@ -132,8 +166,8 @@ const AccountDetailsUpdate = () => {
               </div>
             )}
           </div>
-          <label className="mt-2 text-blue-600 cursor-pointer block text-center">
-            Upload
+          <label className="mt-2 text-sky-800 cursor-pointer block text-center">
+            {uILoading ? "Uploading ..." : "Upload"}
             <input
               type="file"
               accept="image/*"
@@ -146,22 +180,25 @@ const AccountDetailsUpdate = () => {
         {/* Form Section */}
         <div className="w-full">
           <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="col-span-2 flex items-center space-x-4">
-      <div className="w-full">
-        <label className="block text-sm font-medium">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="w-full p-3 border rounded-md"
-          required
-        />
-      </div>
-      <div className="flex items-center justify-center h-full">
-        <BadgeCheck className="text-green-500 cursor-pointer mt-5" size={32} />
-      </div>
-    </div>
+            <div className="col-span-2 flex items-center space-x-4">
+              <div className="w-full">
+                <label className="block text-sm font-medium">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full p-3 border rounded-md"
+                  required
+                />
+              </div>
+              {/* <div className="flex items-center justify-center h-full">
+                <BadgeCheck
+                  className="text-green-500 cursor-pointer mt-5"
+                  size={32}
+                />
+              </div> */}
+            </div>
             <div>
               <label className="block text-sm font-medium">Full Name</label>
               <input
@@ -196,9 +233,9 @@ const AccountDetailsUpdate = () => {
                 <option value="" disabled>
                   Select Gender
                 </option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-                <option value="Other">Other</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
               </select>
             </div>
             <div>
@@ -215,9 +252,9 @@ const AccountDetailsUpdate = () => {
                 <option value="" disabled>
                   Select Marital Status
                 </option>
-                <option value="Married">Married</option>
-                <option value="Unmarried">Unmarried</option>
-                <option value="Single">Single</option>
+                <option value="married">Married</option>
+                <option value="unmarried">Unmarried</option>
+                <option value="single">Single</option>
               </select>
             </div>
             <div>
@@ -231,20 +268,19 @@ const AccountDetailsUpdate = () => {
                 required
               />
             </div>
-            
-  
           </form>
 
-          {/* Save Changes Button - Positioned Below Form Fields */}
           <div className="mt-6 md:ml-16 md:w-60">
-            <button
+            <Button
+              color="primary"
               type="submit"
               onClick={handleSubmit}
-              className="bg-primary-600 text-white px-8 py-4 rounded-md hover:bg-primary-600 w-full md:w-auto"
+              className="text-white px-8 py-4 rounded-md w-full md:w-auto"
               disabled={loading}
+              isLoading={loading}
             >
               {loading ? "Saving..." : "Save Changes"}
-            </button>
+            </Button>
           </div>
         </div>
       </div>
