d6760dc836727ef5e8a1b9db80f15ade33c223d3
@kanhapro <kanhakumarkhatua2001@gmail.com>
2025-04-22 14:06:05 +0530

    refactor: remove unused DigitalPrescription components

--- components/DigitalPrescription/index.ts
+++ components/DigitalPrescription/index.ts
@@ -1,17 +1,3 @@
-import HeroSection from "./HeroSection";
 import UploadPrescription from "./UploadPrescription/UploadPrescription";
-import HowToUseSection from "./HowToUseSection";
-import ReviewsSection from "./ReviewsSection";
-import PrescriptionComparisonSection from "./PrescriptionComparison";
-import SupportSection from "./SupportSection";
-import HealthCalculatorSection from "./HealthCalculatorSection";
 
-export {
-  HeroSection,
-  UploadPrescription,
-  HowToUseSection,
-  ReviewsSection,
-  PrescriptionComparisonSection,
-  SupportSection,
-  HealthCalculatorSection,
-};
+export { UploadPrescription };
